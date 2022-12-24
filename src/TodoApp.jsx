d49bc11218bf84843b814c19e35c5baa8451373b
Aditya Singh <thakuraditya621@gmail.com>
2022-12-24 20:25:03 +0530

    Todo App

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -4,23 +4,35 @@ function TodoApp() {
   const [task, setTask] = useState("");
   const [todos, setTodos] = useState(["Aditya", "ridu"]);
 
-  function addTodos() {
+  function addTodos(e) {
+    e.preventDefault();
     setTodos((oldTodos) => {
+      setTask("");
       return [...oldTodos, task];
     });
+  }
+
+  // for enter the stuff
+  function tryEnter(e) {
+    if (e.keyCode === 13) {
+      addTodos();
+    }
   }
 
   return (
     <div>
       <h1>Best Todo App Till now</h1>
-      <input
-        type="text"
-        value={task}
-        onChange={(event) => {
-          setTask(event.target.value);
-        }}
-      ></input>
-      <button onClick={addTodos}>Add</button>
+      <form onSubmit={addTodos}>
+        <input
+          // onKeyDown={tryEnter}
+          type="text"
+          value={task}
+          onChange={(event) => {
+            setTask(event.target.value);
+          }}
+        ></input>
+        <button type="submit">Add</button>
+      </form>
 
       <ul>
         {todos.map((todo) => {
