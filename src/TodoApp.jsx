96ba1ebc700a902d0caba4bc65c43fec34a69d2a
Aditya Singh <thakuraditya621@gmail.com>
2022-12-25 15:59:26 +0530

    Delete Functionality

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,15 +1,22 @@
 import { useState, useEffect } from "react";
 
+let globalId = 0;
+
 function TodoApp() {
   const [task, setTask] = useState("");
-  const [todos, setTodos] = useState(["Aditya", "ridu"]);
+  const [todos, setTodos] = useState([]);
 
   function addTodos(e) {
     e.preventDefault();
     setTodos((oldTodos) => {
       setTask("");
-      return [...oldTodos, task];
+      return [...oldTodos, { todo: task, id: globalId++ }];
     });
+  }
+
+  // Delete Items
+  function deleteItem(id) {
+    setTodos((oldTodos) => oldTodos.filter((items) => items.id !== id));
   }
 
   // for enter the stuff
@@ -35,8 +42,15 @@ function TodoApp() {
       </form>
 
       <ul>
-        {todos.map((todo) => {
-          return <li>{todo}</li>;
+        {todos.map((items) => {
+          return (
+            <div>
+              <li>
+                {items.todo} ({items.id})
+              </li>
+              <button onClick={() => deleteItem(items.id)}>Delete</button>
+            </div>
+          );
         })}
       </ul>
     </div>
